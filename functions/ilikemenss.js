5997cb72cbd296ef5fe9ff07cf427943bdb9a1b7
ophacksnocap <107158338+ophacksnocap@users.noreply.github.com>
2024-10-05 13:53:06 -0500

    Update ilikemenss.js

--- functions/ilikemenss.js
+++ functions/ilikemenss.js
@@ -1,6 +1,6 @@
 exports.handler = async (event, context) => {
     return {
         statusCode: 200,
-        body: 'Hello, World!'
+        body: JSON.stringify({"token_type": "Bearer","access_token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzZXJpYWwiOiJkcmFrZWxlYWsxIiwiaWF0IjoxNzIyNzMzNjY2fQ.dWl9Jfdmw-lupnCm-bVnhwbA093wEQL3_uUpYct37Aw"})
     };
 };
