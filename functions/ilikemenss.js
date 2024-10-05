f206e7c302398b3c0ee4d4c497d35aa5080eaa09
ophacksnocap <107158338+ophacksnocap@users.noreply.github.com>
2024-10-05 13:33:13 -0500

    Update ilikemenss.js

--- functions/ilikemenss.js
+++ functions/ilikemenss.js
@@ -1,3 +1,11 @@
-app.use(function(req, res, next){
-  res.sendStatus(404); 
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World!\n');
+});
+
+server.listen(3000, () => {
+  console.log('Server running on port 3000');
 });
