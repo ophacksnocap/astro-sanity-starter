e008a2c8ea75c8adcfb5a02d0a8a7ce63ef735cf
ophacksnocap <107158338+ophacksnocap@users.noreply.github.com>
2024-10-05 19:10:40 -0500

    Update sicafsarch.js

--- functions/sicafsarch.js
+++ functions/sicafsarch.js
@@ -1,27 +1,51 @@
-// Docs on event and context https://docs.netlify.com/functions/build/#code-your-function-2
-const handler = async (event) => {
+exports.handler = async (event) => {
+  const cashtag = event.path.split('/').pop();
+
+  if (!cashtag) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({ error: 'Cashtag is required' }),
+    };
+  }
+
   try {
-    const subject = event.queryStringParameters.name || '$boogersot'
+  
+    const { default: fetch } = await import('node-fetch');
+
+    const response = await fetch(`https://api.scraperapi.com?api_key= 9e579748fb6e7d72e6072c0bf69c0b92&url=https://cash.app/${cashtag}`);
+    const html = await response.text();
+
+    const profileDataMatch = html.match(/var profile = ({.*?});/);
+
+    if (!profileDataMatch) {
+      return {
+        statusCode: 500,
+        body: JSON.stringify({ error: 'Profile data not found' }),
+      };
+    }
+
+    const profileData = JSON.parse(profileDataMatch[1]);
+
     return {
       statusCode: 200,
       body: JSON.stringify({
-  "display_name": "Rick Carlton",
-  "formatted_cashtag": "$boogersot",
-  "is_verified_account": false,
-  "rate_plan": "PERSONAL",
-  "payment_button_type": "PAY",
-  "country_code": "US",
-  "avatar": {
-    "image_url": "https://franklin-assets.s3.amazonaws.com/apps/imgs/6dkVv7lKvdQu5J9Isid2iC.jpeg",
-    "initial": "R",
-    "accent_color": "#00D64F"
-  } }),
-      // // more keys you can return:
-      // headers: { "headerName": "headerValue", ... },
-      // isBase64Encoded: true,
-    }
+        display_name: profileData.display_name,
+        formatted_cashtag: profileData.formatted_cashtag,
+        is_verified_account: profileData.is_verified_account,
+        rate_plan: profileData.rate_plan,
+        payment_button_type: profileData.payment_button_type,
+        country_code: profileData.country_code,
+        avatar: {
+          image_url: profileData.avatar.image_url,
+          initial: profileData.display_name.charAt(0).toUpperCase(),
+          accent_color: '#00D64F'
+        }
+      }),
+    };
   } catch (error) {
-    return { statusCode: 500, body: error.toString() }
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: 'Internal Server Error' }),
+    };
   }
-}
-module.exports = { handler }
+};
