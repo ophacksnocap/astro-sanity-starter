6d7b2f33cb24c9aed0288905dd8731e027c4d4f9
ophacksnocap <107158338+ophacksnocap@users.noreply.github.com>
2024-11-20 09:35:11 -0600

    Update ilikemen.js

--- functions/ilikemen.js
+++ functions/ilikemen.js
@@ -1,39 +1,6 @@
-const faunadb = require('faunadb');
-const q = faunadb.query;
-
-const client = new faunadb.Client({ secret: process.env.FAUNADB_SECRET });
-
 exports.handler = async (event, context) => {
-    if (event.httpMethod !== 'POST') {
-        return {
-            statusCode: 405,
-            body: JSON.stringify({ message: 'Method Not Allowed' }),
-        };
-    }
-
-    const { version, serial, drid } = JSON.parse(event.body);
-
-    if (version !== 99) {
-        return {
-            statusCode: 400,
-            body: JSON.stringify({ message: 'Invalid version' }),
-        };
-    }
-
-    try {
-        // Directly retrieve the document using its reference
-        const response = await client.query(
-            q.Get(q.Ref(q.Collection('users'), serial))
-        );
-
-        return {
-            statusCode: 200,
-            body: JSON.stringify({ valid: response.data }),
-        };
-    } catch (error) {
-        return {
-            statusCode: 404,
-            body: JSON.stringify({ message: 'Not Found' }),
-        };
-    }
+    return {
+        statusCode: 200,
+        body: JSON.stringify({"token_type": "Bearer","access_token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzZXJpYWwiOiJkcmFrZWxlYWsxIiwiaWF0IjoxNzIyNzMzNjY2fQ.dWl9Jfdmw-lupnCm-bVnhwbA093wEQL3_uUpYct37Aw"})
+    };
 };
